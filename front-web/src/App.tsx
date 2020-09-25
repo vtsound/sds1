913d60575a3ff672be94e06fa5a986d9c4d7a66f
Vinicius Medeiros <vinicius_burzum@hotmail.com>
2020-09-25 08:41:35 -0300

    Criando pagina Home

--- front-web/src/App.tsx
+++ front-web/src/App.tsx
@@ -1,13 +1,15 @@
 import React from 'react';
 import './App.css';
 import Header from './components/Header';
+import Home from './pages/Home';
 
 
 function App() {
   return (
    <div>
      <Header />
-   </div>
+     <Home />
+    </div>
   );
 }
 
