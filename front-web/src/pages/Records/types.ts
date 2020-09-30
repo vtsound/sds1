4038a2e72cd77438f2d3dd4baffc2d3006948b8e
Vinicius Medeiros <vinicius_burzum@hotmail.com>
2020-09-30 11:38:11 -0300

    Criando página de Listagem dos registros

---
+++ front-web/src/pages/Records/types.ts
@@ -0,0 +1,15 @@
+export type RecordsReponse = {
+    content: RecordItem[];
+    totalPages: number;
+}
+
+export type RecordItem = {
+            id: number;
+            moment: string;
+            name: string;
+            age: number;
+            gameTitle: string;
+            gamePlatform: Platform;
+            genreName: string;
+}
+export type Platform = 'XBOX'| 'PC' | 'PLAYSTATION';  
